befe1744a740b7b34d9d26322425e81f3d73c323
Tom <tom@Toms-Mac-mini.local>
2026-01-04 03:23:16 +0000

    Release version 1.8.5

--- angular-resource.js
+++ angular-resource.js
@@ -493,7 +493,7 @@ function shallowClearAndCopy(src, dst) {
  *
  */
 angular.module('ngResource', ['ng']).
-  info({ angularVersion: '1.8.4' }).
+  info({ angularVersion: '1.8.5' }).
   provider('$resource', function ResourceProvider() {
     var PROTOCOL_AND_IPV6_REGEX = /^https?:\/\/\[[^\]]*][^/]*/;
 
@@ -650,9 +650,20 @@ angular.module('ngResource', ['ng']).
             }
           });
 
-          // strip trailing slashes and set the url (unless this behavior is specifically disabled)
-          if (self.defaults.stripTrailingSlashes) {
-            url = url.replace(/\/+$/, '') || '/';
+          // Strip trailing slashes and set the URL (unless this behavior is specifically disabled)
+          if (self.defaults.stripTrailingSlashes && url.length > 1 && url[url.length - 1] === '/') {
+            // Remove trailing slashes via reverse iteration instead of regex
+            // to avoid possible ReDoS vulnerability (CVE-2023-26117)
+            let endIndex = url.length;
+
+            for (let i = url.length - 1; i >= 0; i--) {
+              if (url[i] !== '/') {
+                break;
+              }
+              endIndex = i;
+            }
+
+            url = url.slice(0, endIndex) || '/';
           }
 
           // Collapse `/.` if found in the last URL path segment before the query.
